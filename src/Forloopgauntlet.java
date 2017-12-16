da5195830018fae299403d9e4a560637a61aa19c
Chumister <league@WTS-IM-17.attlocal.net>
2017-12-16 15:57:57 -0800

    sadf

---
+++ src/Forloopgauntlet.java
@@ -0,0 +1,64 @@
+import java.awt.DisplayMode;
+
+//public class Forloopgauntlet {
+//public static void main(String[] args) {
+//	for (int i = 0; i <101; i++) {
+//System.out.println(i);	
+
+//	}
+//}
+//}
+//public class Forloopgauntlet {
+//	public static void main(String[] args) {
+	//	for (int i = 100; i > 0; i--) {
+	//		System.out.println(i);
+	//	}
+		
+	//}
+	
+	
+	
+//}
+//public class Forloopgauntlet {
+//	public static void main(String[] args) {
+//		for (int i = 0; i < 102; i++) {
+//			if (i%2==0) {
+	//		System.out.println(i);	
+	//		}
+	//	}
+	//}
+	
+	
+	
+//}
+//public class Forloopgauntlet {
+	//public static void main(String[] args) {
+		//for (int i = 1; i < 101; i++) {
+		//	if (i%2==0) {
+		//		System.out.println(i-1);
+	//		}
+			
+//}}
+//}
+//public class Forloopgauntlet {
+	//public static void main(String[] args) {
+	//	for (int i = 0; i < 501; i++) {
+		//	if (i%2==0) {
+		//		System.out.println(i+"even");
+		//	} else {
+//System.out.println(i+"odd");
+		//	}
+	//	}
+//	}
+//}
+public class Forloopgauntlet {
+	public static void main(String[] args) {
+for (int i = 7; i < 777; i++) {
+	if (i%7==0) {
+		System.out.println(i);
+	}
+}
+	
+	
+	
+	}}
